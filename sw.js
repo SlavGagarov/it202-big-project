511886693d0edfc1d029066b70a94cae67fff393
Slav Gagarov <sgagar2@uic.edu>
2020-04-30 15:35:25 +0000

    manifest

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
+    "/",
     '/index.html'
 ];
 
@@ -8,7 +9,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
+
         return cache.addAll(urlsToCache);
       })
   );
